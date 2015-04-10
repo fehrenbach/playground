b321e95cd4d5243962a886b829089c4158f5e9b4
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2015-04-10 11:50:36 +0100

    Stupid blog post...

---
+++ Easier.java
@@ -0,0 +1,11 @@
+/* http://www.thousandtyone.com/blog/EasierThanFizzBuzzWhyCantProgrammersPrint100To1.aspx 
+ * What? This is ridiculous.
+ * Also, what's with the line plot?
+ */
+public class Easier {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 99; i++) {
+            System.out.println(100-i);
+        }
+    }
+}
