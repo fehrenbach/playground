a3ee3f389ecf03025b6ec331bbd4756966c0d00f
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2015-12-09 16:12:44 +0000

    Remove () -> a stuff for delaying execution
    Haskell is lazy, so shouldn't need this, right? Not sure. It seems to
    work fine so far. Let's see how long it will last.

--- hquel/hquel.hs
+++ hquel/hquel.hs
@@ -19,7 +19,9 @@ data Order = Order Int Int Int
 -- products = undefined
 
 -- Questions:
---  - OCaml version uses () -> a for delaying evaluation. Haskell is lazy. Do we still need this?
+--  - OCaml version uses () -> a for delaying evaluation.
+--    Haskell is lazy. Do we still need this?
+--    Apparently not.. as in, I removed it, it still seems to work.
 class Symantics repr where
   -- product :: repr Int -> repr String -> repr Int -> Product
   -- p_pid :: repr Product -> repr Int
@@ -42,17 +44,17 @@ class Symantics repr where
   -- string :: String -> repr String
   -- lam :: (repr a -> repr b) -> repr (a -> b)
   -- app :: repr (a -> b) -> repr a -> repr b
-  for :: (() -> repr [a]) -> (repr a -> repr [b]) -> repr [b]
-  where' :: repr Bool -> (() -> repr [a]) -> repr [a]
+  for :: repr [a] -> (repr a -> repr [b]) -> repr [b]
+  where' :: repr Bool -> repr [a] -> repr [a]
   yield :: repr a -> repr [a]
-  -- nil :: () -> repr [a]
+  nil :: repr [a]
   -- union :: repr [a] -> repr [a] -> repr [a]
   equals :: Eq a => repr a -> repr a -> repr Bool
 
 fortytwo = int 42
 
 q1 :: Symantics repr => repr Int -> repr [Order]
-q1 xoid = for (\() -> orders) $ (\o -> where' (equals (oid o) xoid) $ (\() -> yield o))
+q1 xoid = for orders $ (\o -> where' (equals (oid o) xoid) $ yield o)
 
 data R a = R{eval:: a}
 
@@ -61,13 +63,14 @@ append = mappend
 
 instance Symantics R where
   int = R
-  for :: (() -> R [a]) -> (R a -> R [b]) -> R [b]
-  for tbl f = case tbl () of
+  for :: R [a] -> (R a -> R [b]) -> R [b]
+  for tbl f = case tbl of
                 R []     -> R []
-                R (x:xs) -> R (eval (f (R x)) `append` (eval (for (\() -> R xs) f)))
-  where' (R p) e = if p then e () else R []
+                R (x:xs) -> R (eval (f (R x)) `append` (eval (for (R xs) f)))
+  where' (R p) e = if p then e else R []
   oid (R (Order oid _ _)) = R oid
   yield (R x) = R [x]
+  nil = R []
   orders = R [Order 1 1 5, Order 1 2 5, Order 1 4 2, Order 2 5 10, Order 2 6 20, Order 3 2 50]
   equals (R a) (R b) = R (a == b)
 
