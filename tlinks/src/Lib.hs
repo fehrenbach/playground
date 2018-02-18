06721c58a144376368220359e95dbdae975fa9e5
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2018-02-18 21:50:10 +0000

    I guess I fail the cylon detector, but tracing FOR works

--- tlinks/src/Lib.hs
+++ tlinks/src/Lib.hs
@@ -13,8 +13,8 @@ import Data.Deriving (deriveEq1)
 import Text.PrettyPrint.ANSI.Leijen
 
 pparens :: Bool -> Doc -> Doc
-pparens True = parens
-pparens False = id
+pparens True d = black (char '(') <> d <> black (char ')')
+pparens False d = d
 
 data Kind = KType | KArrow Kind Kind
   deriving (Eq)
@@ -123,6 +123,7 @@ tInstantiate1 = go instantiate1
 data Expr c a x
   = ETrue
   | EVar x
+  | EIf (Expr c a x) (Expr c a x) (Expr c a x)
   | ELam (Type c a) (Scope () (Expr c a) x)
   | EApp (Expr c a x) (Expr c a x)
   | ETLam Kind (Expr (Scope () c) a x)
@@ -131,8 +132,7 @@ data Expr c a x
   | ESingletonList (Expr c a x)
   | EConcat (Expr c a x) (Expr c a x)
   | EFor {- (Type c a) -} (Expr c a x) (Scope () (Expr c a) x)
-  | EEmptyRecord
-  | ERecordExt Label (Expr c a x) (Expr c a x)
+  | ERecord [(Label, Expr c a x)]
   deriving (Functor)
 
 instance (Eq a, Monad c) => Applicative (Expr c a) where
@@ -152,8 +152,8 @@ instance (Eq a, Monad c) => Monad (Expr c a) where
   EEmptyList >>= _ = EEmptyList
   ESingletonList e >>= f = ESingletonList (e >>= f)
   EConcat l r >>= f = EConcat (l >>= f) (r >>= f)
-  EEmptyRecord >>= _ = EEmptyRecord
-  ERecordExt l x r >>= f = ERecordExt l (x >>= f) (r >>= f)
+  ERecord le >>= f = ERecord (map (\(l, x) -> (l, x >>= f)) le)
+  EIf i t e >>= f = EIf (i >>= f) (t >>= f) (e >>= f)
 
 liftCE :: Monad c => Expr c a b -> Expr (Scope () c) a b
 liftCE ETrue = ETrue
@@ -166,6 +166,7 @@ liftCE (EVariant l e) = EVariant l (liftCE e)
 liftCE EEmptyList = EEmptyList
 liftCE (ESingletonList e) = ESingletonList (liftCE e)
 liftCE (EConcat l r) = EConcat (liftCE l) (liftCE r)
+liftCE (EIf i t e) = EIf (liftCE i) (liftCE t) (liftCE e)
 
 liftCT :: Monad c => Type c a -> Type (Scope () c) a
 liftCT TBool = TBool
@@ -181,8 +182,9 @@ efor :: Eq x => x -> Expr c a x -> Expr c a x -> Expr c a x
 efor x i o = EFor i (abstract1 x o)
 
 record :: [(Label, Expr c a x)] -> Expr c a x
-record [] = EEmptyRecord
-record ((l, x) : r) = ERecordExt l x (record r)
+-- record [] = EEmptyRecord
+-- record ((l, x) : r) = ERecordExt l x (record r)
+record = ERecord
 
 idA :: Expr Constructor String String
 idA = elam "ex" (TT (CVar "a")) (EVar "ex")
@@ -238,13 +240,16 @@ prettyExpr ns p (ESingletonList e) = brackets $ prettyExpr ns False e
 prettyExpr ns p (EConcat l r) = pparens p $ prettyExpr ns True l <+> text "++" <+> prettyExpr ns True r
 prettyExpr (v:vs, tvs) p (EFor i o) = pparens p $ hang 2 $
   bold (text "for") <+> parens (text v <+> text "<-" <+> prettyExpr (v:vs, tvs) False i) </> prettyExpr (vs, tvs) False (instantiate1 (EVar v) o)
-prettyExpr ns p EEmptyRecord = braces empty
-prettyExpr ns p r@(ERecordExt _ _ _) = braces $ align $ printRecord ns r
+prettyExpr ns _ (ERecord []) = braces empty
+prettyExpr ns _ (ERecord l) = group $ char '{' <> (align $ printRec ns l)
+prettyExpr ns p (EIf c t e) = pparens p $ group $
+  bold (text "if") <+> prettyExpr ns False c </>
+  bold (text "then") <+> prettyExpr ns False t </>
+  bold (text "else") <+> prettyExpr ns False e
 
-printRecord ns EEmptyRecord = empty
-printRecord ns (ERecordExt l x EEmptyRecord) = blue (text l) <+> char '=' <+> prettyExpr ns False x
-printRecord ns (ERecordExt l x r) = blue (text l) <+> char '=' <+> prettyExpr ns False x <> char ',' </> printRecord ns r
-
+printRec ns [] = char '}'
+printRec ns [(l,x)] = blue (text l) <+> char '=' <+> prettyExpr ns False x <> char '}'
+printRec ns ((l,x):r) = blue (text l) <+> char '=' <+> prettyExpr ns False x <> char ',' </> printRec ns r
 {-
 
 -- deriveShow1 ''Type
@@ -262,23 +267,53 @@ evars = ["x", "y", "z"] <> [ "x" <> show x | x <- [0..] ]
 ttype :: Constructor String -> Type Constructor String
 ttype c = TArrow (TT (CApp (CVar "TRACE") c)) (TT (CApp (CVar "TRACE") c))
 
-trace :: Expr Constructor String String -> Expr Constructor String String
-trace ETrue = elam "t" (ttype CBool) (EApp (EVar "t") (EVariant "Lit" ETrue))
-trace (EVar x) = elam "t" (ttype (CVar "??")) (EApp (EVar "t") (EVar x))
-trace (ELam _ _) = error "cannot trace lambda"
+-- trace :: Expr Constructor String String -> Expr Constructor String String
+trace' :: Type c a -> Expr c a x -> Expr c a x
+trace' ct ETrue = ELam ct $ toScope (EApp (EVar (B ())) (EVariant "Lit" ETrue))
+trace' ct (EVar x) = ELam ct $ toScope (EApp (EVar (B ())) (EVar (F x)))
+trace' _ (ETLam _ _) = error "cannot trace Lambda"
+trace' _ (EApp _ _) = error "cannot trace app"
+trace' _ (EVariant _ _) = error "cannot trace variant constructor"
+trace' ct EEmptyList = ELam ct $ toScope $ EEmptyList
+trace' ct (ESingletonList e) = ELam ct $ toScope $
+  ESingletonList (EApp (fmap F (trace' ct e)) (EVar (B ())))
+trace' ct (ERecord l) = ELam ct $ toScope $
+  ERecord (map (\(l, x) -> (l, EApp (fmap F $ trace' ct x) (EVar (B ())))) l)
+trace' ct (EFor m n) = ELam ct $ toScope $
+  EFor (EApp (fmap F (trace' ct m)) (ELam ct (toScope (EVar (B ()))))) $ toScope $
+  -- Right, so this fmap is where we keep the "same" variable that was
+  -- bound in the body before.
+    EApp (fmap (\x -> case x of F x -> F (F x); B () -> B ()) (trace' ct (fromScope n))) $
+         ELam ct $ toScope $ EApp (EVar (F (F (B ())))) (EVariant "For" (record [("in", EVar (F (B ()))), ("out", EVar (B ()))]))
+
+trace = trace' TBool
+
+-- trace (EVar x) = elam "t" (ttype (CVar "??")) (EApp (EVar "t") (EVar x))
+-- trace (ELam _ _) = error "cannot trace lambda"
+{-
 trace (ETLam _ _) = error "cannot trace Lambda"
 trace (EApp _ _) = error "cannot trace app"
 trace (EVariant _ _) = error "cannot trace variant constructor"
 trace EEmptyList = elam "t" (ttype (CVar "??")) EEmptyList
 trace (ESingletonList e) = elam "t" (ttype (CVar "??")) (ESingletonList (EApp (trace e) (EVar "t")))
 trace (EConcat l r) = elam "t" (ttype (CVar "??")) (EConcat (efor "l" (EApp (trace l) (EVar "t")) (ESingletonList (EVar "l"))) (efor "r" (EApp (trace r) (EVar "t")) (ESingletonList (EVar "r"))))
-trace (EFor i o) = -- not sure this makes any sense
- elam "t" (ttype (CVar "??")) $
-  efor "i" (EApp (trace i) (elam "x" (TT (CVar "???")) (EVar "x"))) $ {- TODO replace by proper identity function -}
-    (EApp (trace (instantiate (const (EVar "i")) o))
-      (elam "o" (TT (CVar "????")) (EApp (EVar "t") (EVariant "For" (record [("in", EVar "i"), ("out", EVar "o")])))))
-
-betaReduce :: Eq x => Monad c => Expr c x a -> Expr c x a
+trace (EIf c t e) = elam "t" (ttype (CVar "??")) $
+  EIf (EApp (EVar "value") (EApp (trace c) idA)) -- TODO replace by proper identity function
+    (EApp (trace t) (elam "m" (TT (CVar "???")) (EApp (EVar "t") (EVariant "IfThen" (record [("cond", EApp (trace c) idA), ("then", EVar "m")])))))
+    (EApp (trace e) (elam "n" (TT (CVar "???")) (EApp (EVar "t") (EVariant "IfElse" (record [("cond", EApp (trace c) idA), ("else", EVar "n")])))))
+trace (EFor i o) = ELam (ttype (CVar "??")) $ toScope $
+  EFor (fmap F (trace i)) $
+    toScope $ EApp (_ o) $
+      ELam (TT (CVar "????")) $ toScope $ EApp (EVar _t) (EVariant "For" (record [("in", _i), ("out", _o)]))
+ -- elam "t" (ttype (CVar "??")) $
+ --   EFor (EApp (trace i) idA) $ {- TODO replace by proper identity function -}
+ --     toScope $ EApp (_ o) $  "o" (TT (CVar "????")) (EApp (EVar "t") (EVariant "For" (record [("in", B ()), ("out", EVar "o")])))
+trace EEmptyRecord = elam "t" (ttype (CVar "??")) EEmptyRecord
+trace (ERecordExt l x r) = elam "t" (ttype (CVar "??")) $
+  ERecordExt l (EApp (trace x) (EVar "t"))
+    (EApp (trace r) (EVar "t"))
+-}
+betaReduce :: Eq a => Monad c => Expr c a x -> Expr c a x
 betaReduce ETrue = ETrue
 betaReduce (EApp (ELam _ b) x) = instantiate1 x b
 betaReduce (EApp f x) = EApp (betaReduce f) (betaReduce x)
@@ -290,6 +325,17 @@ betaReduce (ESingletonList e) = ESingletonList (betaReduce e)
 betaReduce EEmptyList = EEmptyList
 betaReduce (EConcat l r) = EConcat (betaReduce l) (betaReduce r)
 betaReduce (EFor i o) = EFor (betaReduce i) (hoistScope betaReduce o)
+betaReduce (EIf c t e) = EIf (betaReduce c) (betaReduce t) (betaReduce e)
+betaReduce (ERecord l) = ERecord (map (\(l, x) -> (l, betaReduce x)) l)
+
+betaReduceN :: Eq a => Monad c => Int -> Expr c a x -> Expr c a x
+betaReduceN 0 e = e
+betaReduceN n e = betaReduceN (n-1) (betaReduce e)
+
+putE :: Expr Constructor String String -> IO ()
+putE e = do
+  putDoc $ prettyExpr (evars, tvars) False e
+  putStrLn ""
 
 someFunc :: IO ()
 someFunc = do
@@ -301,27 +347,20 @@ someFunc = do
   -- putStrLn ""
   -- putDoc $ prettyExpr (evars, tvars) False polyId
   -- putStrLn ""
-  -- putDoc $ prettyExpr (evars, tvars) False (trace ETrue)
-  -- putStrLn ""
-  -- putDoc $ prettyExpr (evars, tvars) False (trace (EVar "foo"))
-  -- putStrLn ""
-  -- putDoc $ prettyExpr (evars, tvars) False (trace EEmptyList)
-  -- putStrLn ""
-  -- putDoc $ prettyExpr (evars, tvars) False (trace (ESingletonList ETrue))
-  -- putStrLn ""
-  -- putDoc $ prettyExpr (evars, tvars) False (trace (ESingletonList (ESingletonList ETrue)))
-  -- putStrLn ""
-  -- putDoc $ prettyExpr (evars, tvars) False (trace (EConcat (ESingletonList ETrue) EEmptyList))
-  -- putStrLn ""
-  -- putDoc $ prettyExpr (evars, tvars) False $ efor "x" (EVar "inList") (ESingletonList (EVar "x"))
-  -- putStrLn ""
-  -- putDoc $ prettyExpr (evars, tvars) False (trace (efor "x" (EVar "inList") (ESingletonList (EVar "x"))))
-  -- putStrLn ""
-  -- putDoc $ prettyExpr (evars, tvars) False ((betaReduce . betaReduce . betaReduce) (trace (efor "x" (EVar "inList") (ESingletonList (EVar "x")))))
-  -- putStrLn ""
-  putDoc $ prettyExpr (evars, tvars) False (efor "x" (EVar "as") (efor "y" (EVar "bs") (EConcat (ESingletonList (EVar "x")) (ESingletonList (EVar "y")))))
+  putDoc $ prettyExpr (evars, tvars) False (trace ETrue)
   putStrLn ""
-  putDoc $ prettyExpr (evars, tvars) False (trace (efor "x" (EVar "as") (efor "y" (EVar "bs") (EConcat (ESingletonList (EVar "x")) (ESingletonList (EVar "y"))))))
+  putDoc $ prettyExpr (evars, tvars) False (trace (EVar "foo"))
   putStrLn ""
-  putDoc $ prettyExpr (evars, tvars) False ((betaReduce . betaReduce . betaReduce . betaReduce . betaReduce . betaReduce . betaReduce . betaReduce)  (EApp (trace (efor "x" (EVar "xs") (efor "y" (EVar "ys") (EConcat (ESingletonList (EVar "x")) (ESingletonList (EVar "y")))))) idA))
+  putDoc $ prettyExpr (evars, tvars) False (trace EEmptyList)
   putStrLn ""
+  putDoc $ prettyExpr (evars, tvars) False (trace (ESingletonList ETrue))
+  putStrLn ""
+  putDoc $ prettyExpr (evars, tvars) False (betaReduceN 4 (trace (ESingletonList (ESingletonList ETrue))))
+  putStrLn ""
+  let forNested = efor "a" (EVar "as") (efor "b" (EVar "bs") (ESingletonList (record [("a", EVar "a"), ("b", EVar "b")])))
+  putE forNested
+  putE $ betaReduceN 9 $ EApp (trace forNested) idA
+  let forIf = efor "x" (EConcat (EVar "xs") (EVar "ys")) (EIf ETrue (ESingletonList (EVar "x")) EEmptyList)
+  putE forIf
+  putE $ trace forIf
+  putE $ betaReduceN 9 $ EApp (trace forIf) idA
